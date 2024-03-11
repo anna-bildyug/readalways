960ebc01a5c1eaa41905552215d5cac59e03b85d
Anna Bildyug <ann.s.skiba@gmail.com>
2024-03-11 21:29:42 +0100

    Init

---
+++ readalways/src/main/java/com/annabildziuh/readalways/ReadalwaysApplication.java
@@ -0,0 +1,13 @@
+package com.annabildziuh.readalways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadalwaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadalwaysApplication.class, args);
+	}
+
+}
---
+++ readalways/src/test/java/com/annabildziuh/readalways/ReadalwaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annabildziuh.readalways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadalwaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
